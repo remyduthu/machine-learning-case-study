29785e4886f9024f2c53c82d6dfc6833f3c5656d
Remy DUTHU <remy.duthu@gmail.com>
2021-03-12 11:32:34 +0100

    Fetch the SVHN dataset w/ a Shell script

---
+++ svhn/data/fetch-dataset.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# Download the dataset
+curl --remote-name http://ufldl.stanford.edu/housenumbers/train_32x32.mat
+curl --remote-name http://ufldl.stanford.edu/housenumbers/test_32x32.mat
