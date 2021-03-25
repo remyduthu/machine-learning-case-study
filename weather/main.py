c302afea68e33dc52117104f2b91627d7332b9ff
Remy DUTHU <remy.duthu@gmail.com>
2021-03-25 16:07:34 +0100

    Create models and make predictions

--- weather/main.py
+++ weather/main.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
 from matplotlib import pyplot
 from os import getcwd, listdir
 from pandas import concat, read_csv, to_datetime
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+
+
+# Utilities
 
 
 def load_df(station_id: int):
@@ -23,64 +32,175 @@ def load_df(station_id: int):
     # Filter based on the station ID
     df.query(f"numer_sta == {station_id}", inplace=True)
 
-    # Convert the date into datetime Python objects
-    df["date"] = to_datetime(df["date"], format="%Y%m%d%H%M%S")
+    # Convert the dates into Pandas Timestamps
+    df["date"] = to_datetime(df["date"], format="%Y%m%d%H%M%S").astype(int) / 10 ** 9
 
     return df
 
 
-def load_dataset(df, param: str):
+def load_ds(df, param: str):
     # Keep only the date and the parameter columns
     df = df[["date", param]]
 
+    # Drop rows with NaN values
+    df = df.dropna()
+
     # Convert into a Numpy array
-    return df.to_numpy()
+    ds = df.to_numpy()
+
+    # Reshape X as we work on a single feature
+    X = ds[:, 0].reshape(-1, 1)
+    y = ds[:, 1]
+
+    return train_test_split(X, y)
 
 
-def plot_dataset(position, dataset: list, param: str):
-    # Plot the dataset values
-    position.plot(dataset[:, 0], dataset[:, 1], "o")
+def plot_ds(position, param, dataset, y_pred):
+    X_train, X_test, y_train, y_test = dataset
 
-    # position.set_xlabel("Date")
-    position.set_ylabel(param)
+    # Add the training data
+    position.scatter(
+        ts_to_date(X_train),
+        y_train,
+        alpha=0.1,
+        label="Training",
+    )
+
+    # Add the test data
+    position.scatter(ts_to_date(X_test), y_test, alpha=0.4, label="Test")
+
+    # Add the predictions
+    position.scatter(ts_to_date(X_test), y_pred, alpha=0.4, label="Predictions")
+
+    position.set(
+        xlabel="Date",
+        ylabel=param,
+    )
+
+    position.legend()
+
+
+def ts_to_date(X: list):
+    ts_list = X[:, 0]
+
+    return [datetime.fromtimestamp(ts) for ts in ts_list]
+
+
+# Models
+
+
+def make_temperature_model(df, position):
+    # Load the temperature dataset
+    t = load_ds(df, "t")
+    X_train, X_test, y_train, y_test = t
+
+    # Build & fit the model
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+
+    plot_ds(
+        position,
+        "Température (K)",
+        dataset=t,
+        y_pred=model.predict(X_test),
+    )
+
+    return model.score(X_test, y_test) * 100
+
+
+def make_humidity_model(df, position):
+    h = load_ds(df, "u")
+    X_train, X_test, y_train, y_test = h
+
+    # Build & fit the model
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+
+    plot_ds(
+        position,
+        "Humidité (%)",
+        dataset=h,
+        y_pred=model.predict(X_test),
+    )
+
+    return model.score(X_test, y_test) * 100
+
+
+# TODO: Try to use another type of Regressor
+def make_wind_direction_model(df, position):
+    wd = load_ds(df, "dd")
+    X_train, X_test, y_train, y_test = wd
+
+    # Build & fit the model
+    model = make_pipeline(
+        PolynomialFeatures(degree=4),
+        StandardScaler(),
+        LinearRegression(),
+    )
+    model.fit(X_train, y_train)
+
+    plot_ds(
+        position,
+        "Direction du vent moyen 10 mn (degré)",
+        dataset=wd,
+        y_pred=model.predict(X_test),
+    )
+
+    return model.score(X_test, y_test) * 100
+
+
+def make_wind_speed_model(df, position):
+    ws = load_ds(df, "ff")
+    X_train, X_test, y_train, y_test = ws
+
+    # Build & fit the model
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+
+    plot_ds(
+        position,
+        "Vitesse du vent moyen 10 mn (m/s)",
+        dataset=ws,
+        y_pred=model.predict(X_test),
+    )
+
+    return model.score(X_test, y_test) * 100
+
+
+def make_atmospheric_pressure_model(df, position):
+    p = load_ds(df, "pres")
+    X_train, X_test, y_train, y_test = p
+
+    # Build & fit the model
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+
+    plot_ds(
+        position,
+        "Pression station (Pa)",
+        dataset=p,
+        y_pred=model.predict(X_test),
+    )
+
+    return model.score(X_test, y_test) * 100
 
 
 def main():
+    # Load the whole data from the CSV files
     print("⚙️  Load the data...")
-    df = load_df(station_id=7630)
+    # df = load_df(station_id=7630)
+    df = load_df(station_id=7460)
 
+    # Create the main figure
     _, axis = pyplot.subplots(5, figsize=(18, 28))
 
-    plot_dataset(
-        axis[0],
-        load_dataset(df, "t"),
-        "Température (K)",
-    )
+    print(f"Temperature: {make_temperature_model(df, axis[0])}%")
+    print(f"Humidity: {make_humidity_model(df, axis[1])}%")
+    print(f"Wind direction: {make_wind_direction_model(df, axis[2])}%")
+    print(f"Wind speed: {make_wind_speed_model(df, axis[3])}%")
+    print(f"Atmospheric pressure: {make_atmospheric_pressure_model(df, axis[4])}%")
 
-    plot_dataset(
-        axis[1],
-        load_dataset(df, "u"),
-        "Humidité (%)",
-    )
-
-    plot_dataset(
-        axis[2],
-        load_dataset(df, "dd"),
-        "Direction du vent moyen 10 mn (degré)",
-    )
-
-    plot_dataset(
-        axis[3],
-        load_dataset(df, "ff"),
-        "Vitesse du vent moyen 10 mn (m/s)",
-    )
-
-    plot_dataset(
-        axis[4],
-        load_dataset(df, "pres"),
-        "Pression station (Pa)",
-    )
-
+    pyplot.tight_layout()
     pyplot.savefig(f"data/dataset.png")
 
 
